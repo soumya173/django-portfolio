dfe43ed47996deff88705b22c7ea9436994011f9
Soumyajit Gorai <soumya173@gmail.com>
2021-01-30 22:46:04 +0530

    Add resume page
    - Custom style for css
    - Resume page

--- projectportfolio/appportfolio/urls.py
+++ projectportfolio/appportfolio/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('resume', views.resume, name='resume'),
 ]
--- projectportfolio/appportfolio/views.py
+++ projectportfolio/appportfolio/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'index.html', context)
+def resume(request):
+    context = {}
+    return render(request, 'resume.html', context)
